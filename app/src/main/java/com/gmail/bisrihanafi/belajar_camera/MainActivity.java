380b30d0f8c3d8dab1b327f787efda9445af013b
Bisri Hanafi <bisrihanafi@gmail.com>
2019-05-14 23:08:25 +0700

    Tambah Simpan ke External Storage

--- app/src/main/java/com/gmail/bisrihanafi/belajar_camera/MainActivity.java
+++ app/src/main/java/com/gmail/bisrihanafi/belajar_camera/MainActivity.java
@@ -2,6 +2,8 @@ package com.gmail.bisrihanafi.belajar_camera;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,17 +12,21 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
     ImageView iv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        cekBaca();
+        cekTulis();
         //TODO 1 : mengaitkan object dengan komponen view
         Button camera= (Button) findViewById(R.id.open);
         iv=(ImageView)findViewById(R.id.img1);
-
         //TODO 2 : membuat event untuk tombol open untuk membuka kamera
         camera.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -43,9 +49,57 @@ public class MainActivity extends AppCompatActivity {
             //TODO 7 : tahap penampil gambar
             Bitmap bi = (Bitmap) data.getExtras().get("data");
             iv.setImageBitmap(bi);
+            //TODO 8 : bagaian ini memanggil method untuk menyimpan gambar yang diambil kedalam memori external
+            SaveImage(bi);
         }else if (resultCode==RESULT_CANCELED){
+            //TODO 9 : menampilkan pesan jika kamera tidak jadi digunakan
             Toast.makeText(this, "kamera tidak jadi digunakan", Toast.LENGTH_LONG).show();
         }
+    }
+    private void SaveImage(Bitmap finalBitmap) {
+        //TODO 10 : Bagian ini mengarahkan lokasi penyimpanan ke direktori external Picture, jika tidak ada folder tersebut maka akan dibuat saat itu juga
+        File myDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).toString());
+        if (!myDir.exists()) {
+            myDir.mkdirs();
+        }
+
+        //TODO 11 : menampilkan lokasi penyimpanan dalam bentuk Toast
+        Toast.makeText(this, myDir.toString(), Toast.LENGTH_LONG).show();
+
+        //TODO 12 memberi nama file secara acak
+        Random generator = new Random();
+        int n = 10000;
+        n = generator.nextInt(n);
+        File file = new File (myDir,  "Image-"+ n +".jpg");
+        if (file.exists ())
+            file.delete ();
+
+        //TODO 13 : memasukan data gambar kedalam file yang telah dibuat
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+            //TODO 14 : mengkopres data kedalam file jpg ter kompres (seperti yang kita tahu format jpg merupakan fil gambar ter kompresi)
+            finalBitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
+            out.flush();
+            //TODO 15 : menutup file
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    //Ini adalah method untuk mengecek apkah external storage dapat ditulis atau tidak
+    public void cekTulis() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            Toast.makeText(this,"Dapat ditulis",Toast.LENGTH_LONG).show();
+        }else Toast.makeText(this,"Tidak dapat ditulis",Toast.LENGTH_LONG).show();
+    }
+    //Ini adalah method untuk mengecek apkah external storage dapat dibaca atau tidak
+    public void cekBaca() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            Toast.makeText(this,"Dapat dibaca",Toast.LENGTH_LONG).show();
+        }else Toast.makeText(this,"Tidak dapat dibaca",Toast.LENGTH_LONG).show();
     }
 
 
